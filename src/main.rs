26ff88f459be9a82d432e372d9aae8575e4f3d46
Laura <rw21062006@gmail.com>
2025-04-06 19:41:15 +0200

    read the file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+
+    let contents = fs::read_to_string("data.txt")
+        .expect("Something went wrong data.txt");
+
+    println!("With text:\n{contents}");
 }
