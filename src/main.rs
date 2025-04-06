908f62a3995d60122aadf7e69f57612c8f707735
Laura <rw21062006@gmail.com>
2025-04-06 22:19:37 +0200

    match to data

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{default, fs};
-use std::thread::spawn;
+use std::fs;
 use eframe::egui;
-use cli_clipboard::get_contents;
 use inputbot::KeybdKey::*;
+use str_distance::*;
 
 struct ClipboardKeyValueDisplay {
     pairs: Vec<(String, String)>,
@@ -22,7 +21,21 @@ impl Default for ClipboardKeyValueDisplay {
 
 impl eframe::App for ClipboardKeyValueDisplay {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.key = cli_clipboard::get_contents().unwrap();
+        self.key = cli_clipboard::get_contents().unwrap_or(self.key.clone());
+
+        let mut min_levenshtein_distance = f64::MAX;
+        let mut min_levenshtein_value = String::new();
+        for (k, v) in &self.pairs {
+            let distance = str_distance_normalized(&self.key, k, Levenshtein::default());
+            if distance < min_levenshtein_distance {
+                min_levenshtein_distance = distance;
+                min_levenshtein_value = v.clone();
+                if distance == 0.0 {
+                    break;
+                }
+            }
+        }
+        self.value = min_levenshtein_value;
 
         egui::CentralPanel::default().frame(egui::Frame::NONE).show(ctx, |ui| {
             ui.label(self.value.clone());
@@ -39,9 +52,17 @@ fn main() {
 
     let contents = fs::read_to_string("data.txt")
         .expect("Something went wrong data.txt");
-    let mut lines = contents.lines();
+    let lines = contents.lines();
+    let mut pairs: Vec<(String, String)> = Vec::new();
 
-    println!("First line:{}\n", lines.next().unwrap_or_default());
+    for line in lines {
+        let penis = line.replace("\\n", "\n");
+        let mut parts = penis.split(';');
+        let key = parts.next().unwrap();
+        let mut value = parts.fold(String::new(), |a, b| a + b + ";");
+        value.pop();
+        pairs.push((key.to_string(), value.to_string()));
+    }
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
@@ -54,7 +75,7 @@ fn main() {
     };
 
     let clipboard_object = ClipboardKeyValueDisplay {
-        pairs: Vec::new(),
+        pairs,
         key: String::new(),
         value: String::new(),
     };
