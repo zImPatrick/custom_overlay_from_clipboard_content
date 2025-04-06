76db6f61fd1613b81d5286772efb36370540032e
Laura <rw21062006@gmail.com>
2025-04-06 20:53:47 +0200

    Fortnite

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use std::fs;
+use eframe::egui;
 
-fn main() {
+struct ClipboardKeyValueDisplay {
+    key: String,
+    value: String,
+}
+
+impl Default for ClipboardKeyValueDisplay {
+    fn default() -> Self {
+        Self {
+            key: String::new(),
+            value: "Fortnite".to_string(),
+        }
+    }
+}
+
+impl eframe::App for ClipboardKeyValueDisplay {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().frame(egui::Frame::NONE).show(ctx, |ui| {
+            ui.label(self.value.clone());
+        });
+    }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        egui::Rgba::TRANSPARENT.to_array()
+    }
+}
+
+fn main() -> eframe::Result<()> {
     println!("Hello, world!");
 
     let contents = fs::read_to_string("data.txt")
         .expect("Something went wrong data.txt");
+    let mut lines = contents.lines();
 
-    println!("With text:\n{contents}");
+    println!("First line:{}\n", lines.next().unwrap_or_default());
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_transparent(true)
+            .with_decorations(false)
+            .with_drag_and_drop(false),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Clipboard Key-Value Display",
+        options,
+        Box::new(|_cc| {
+            Ok(Box::<ClipboardKeyValueDisplay>::default())
+        }),
+    )
 }
