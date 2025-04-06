65844ba46adc619edf1f37dd52411e3d20b87b31
Laura <rw21062006@gmail.com>
2025-04-06 21:54:41 +0200

    read keyboard input

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom Overlay from Clipboard Content
+To compile this on Debian install `libxtst-dev libx11-dev libxtst-dev libudev-dev`.
+Also if you're on Linux and want key reading you have to run as sudo.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::fs;
+use std::{default, fs};
+use std::thread::spawn;
 use eframe::egui;
+use cli_clipboard::get_contents;
+use inputbot::KeybdKey::*;
 
 struct ClipboardKeyValueDisplay {
+    pairs: Vec<(String, String)>,
     key: String,
     value: String,
 }
@@ -9,6 +13,7 @@ struct ClipboardKeyValueDisplay {
 impl Default for ClipboardKeyValueDisplay {
     fn default() -> Self {
         Self {
+            pairs: Vec::new(),
             key: String::new(),
             value: "Fortnite".to_string(),
         }
@@ -17,17 +22,19 @@ impl Default for ClipboardKeyValueDisplay {
 
 impl eframe::App for ClipboardKeyValueDisplay {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.key = cli_clipboard::get_contents().unwrap();
+
         egui::CentralPanel::default().frame(egui::Frame::NONE).show(ctx, |ui| {
             ui.label(self.value.clone());
         });
     }
 
-    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
-        egui::Rgba::TRANSPARENT.to_array()
-    }
+    // fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+    //     egui::Rgba::TRANSPARENT.to_array()
+    // }
 }
 
-fn main() -> eframe::Result<()> {
+fn main() {
     println!("Hello, world!");
 
     let contents = fs::read_to_string("data.txt")
@@ -40,15 +47,29 @@ fn main() -> eframe::Result<()> {
         viewport: egui::ViewportBuilder::default()
             .with_transparent(true)
             .with_decorations(false)
-            .with_drag_and_drop(false),
+            .with_drag_and_drop(false)
+            .with_always_on_top()
+            .with_mouse_passthrough(true),
         ..Default::default()
     };
 
-    eframe::run_native(
+    let clipboard_object = ClipboardKeyValueDisplay {
+        pairs: Vec::new(),
+        key: String::new(),
+        value: String::new(),
+    };
+    
+    FKey.bind(move || {
+        println!("FKey pressed");
+    });
+
+    std::thread::spawn(inputbot::handle_input_events);
+
+    let _ = eframe::run_native(
         "Clipboard Key-Value Display",
         options,
         Box::new(|_cc| {
-            Ok(Box::<ClipboardKeyValueDisplay>::default())
+            Ok(Box::new(clipboard_object))
         }),
-    )
+    );
 }
